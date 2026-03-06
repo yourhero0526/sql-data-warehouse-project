a54e2c2b0a5dd38696099ab9d680843954b4052a
yourhero0526 <rda.delapena@gmail.com>
2026-03-07 03:57:45 +0800

    Update 02_create_tables.sql

--- scripts/02_create_tables.sql
+++ scripts/02_create_tables.sql
@@ -43,9 +43,9 @@ CREATE TABLE bronze.crm_sales_details(
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt DATE,
-	sls_ship_dt DATE,
-	sls_due_dt DATE,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT
